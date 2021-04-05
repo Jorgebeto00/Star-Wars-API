01dafb3d3e9b64a9546722c13bcc1fa2f7822f32
Jorge R <jorgebeto00@protonmail.com>
2021-04-05 02:54:42 +0000

    version 0.1

--- src/models.py
+++ src/models.py
@@ -1,19 +1,59 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from eralchemy import render_er
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    created_at = Column(String(250), nullable=False)
+    updated_at = Column(String(250), nullable=True)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Planets(Base):
+    __tablename__ = 'planets'
+   
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(250), nullable=False)
+    planet_climate = Column(String(250), nullable=True)
+    planet_diameter = Column(Integer, nullable=True)
+    planet_population = Column(Integer, nullable=True)
+    planet_gravity = Column(Integer, nullable=True)
+    planet_terrain = Column(String(250), nullable=True)
+    planet_url = Column(String(250), nullable=True)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Characters(Base):
+    __tablename__ = 'characters'
+   
+    id = Column(Integer, primary_key=True)
+    characters_name = Column(String(250), nullable=False)
+    characters_gender = Column(String(250), nullable=True)
+    characters_birthyear = Column(String(250), nullable=True)
+    characters_eyecolor = Column(String(250), nullable=True)
+    characters_haircolor = Column(String(250), nullable=True)
+    characters_homeworld = Column(String(250), nullable=True)
+    characters_height = Column(Integer, nullable=True)
+    planet_url = Column(String(250), nullable=True)
+
+class User_Favorites(Base):
+    __tablename__ = 'user_favorites'
+   
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    planets_id = Column (Integer, ForeignKey('planets.id'))
+
+def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+render_er(Base, 'diagram.png')
